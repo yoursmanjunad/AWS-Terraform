89081bc56ffe6c34942b148e7ea25eccc788c982
Manjunath Irukulla <imanjunad@gmail.com>
2024-08-21 09:16:38 +0530

    Adding infra files

--- datasources.tf
+++
@@ -1,9 +0,0 @@
-data "aws_ami" "server_ami" {
-  most_recent = true
-  owners      = ["137112412989"]
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hbm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
-  }
-}
